77db06a4d803c77fef7f4103bb39921c5ba1be18
MonikySouzaLopes <monikysouzalopes@gmail.com>
2024-02-28 14:31:36 -0400

    Atualizado 28/02

--- src/app/componentes/Footer.js
+++ src/app/componentes/Footer.js
@@ -1,6 +1,9 @@
+import styles from "./footer.module.css";
+import Image from "next/image";
 export default function Footer(){
     return(
           <footer>
+            <Image width={100} height={100} src={"https://ead.ifms.edu.br/theme/moove/pix/moodle-logo-white.png"}/>
             <h1>Rodapé</h1>
           </footer>
     );
